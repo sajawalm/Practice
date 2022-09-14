e47f7253f7b088f82c53c2ef62c02121f2e55f13
Sajawal Mehboob <sajawalmehboob157@gmail.com>
2022-09-14 16:37:20 +0500

    Practice

---
+++ conditionals.py
@@ -0,0 +1,58 @@
+#x = int(input(" what's x? "))
+#y = int(input("what's y? "))
+#if x != y :
+# print("x is not equal to y")
+#else:
+#print("x is equal to y")
+#score = int(input("scores: "))
+#   print("Grade: C")
+#if score >= 90:
+#   print("Grades: A")
+#elif score >=80:
+ #   print("Grades:B ")
+#elif score >= 70:
+#    print("Grades:C ")
+#elif score >= 60:
+#    print("Grades:D ")
+#else:
+#    print("Grades: F")
+#
+# = int(input(" what's x? "))
+#if x % 2 == 0:
+#    print("even")
+#else:
+#    print("odd")
+#simple example
+cars = ["audi", "bmw","toyota","honda"]
+for car in cars:
+    if car == "toyota":
+        print(car.upper())
+    else:
+        print(car.title())
+car = "audi"
+car == "bmw"
+print(car)
+requested_topping = "mushrooms"
+if requested_topping != "cavi":
+    print("hold the cavi")
+#age = int(input("age: "))
+#if age != 42 :
+#    print("this is not the correct answer")
+#
+age =int(input("age:"))
+if age <=4:
+    price =0
+elif age <18:
+    price = 25
+else:
+    price = 40
+print(f"you entry fee is{price}")
+
+
+
+
+
+
+
+
+
